8460990970fe72b3478ac4d76167dc225cc09232
unknown <Tajveed.Aslam@MKS.COM>
2019-10-31 15:29:50 +0500

    broken homepage into smaller components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './directory.styles.scss';
+import MenuItem from '../menu-items/menu-items.component';
+
+class Directory extends Component {
+    constructor() {
+        super();
+        this.state = {
+            sections: [
+                {
+                    title: 'shirts',
+                    imageUrl: 'https://static-01.daraz.pk/p/99ef60cd2838dd9a714fe446733e85aa.jpg',
+                    id: 1,
+                    linkUrl: 'shop/Shirts'
+                },
+                {
+                    title: 'jackets',
+                    imageUrl: 'https://static-01.daraz.pk/p/5e5002da644ce668ae1dc7e4b44b6e6f.jpg',
+                    id: 2,
+                    linkUrl: 'shop/jackets'
+                },
+                {
+                    title: 'sneakers',
+                    imageUrl: 'https://static-01.daraz.pk/p/c564c2d0e3c05cd3cd8e29c1bb4c31b8.jpg',
+                    id: 3,
+                    linkUrl: 'shop/sneakers'
+                },
+                {
+                    title: 'womens',
+                    imageUrl: 'https://static-01.daraz.pk/p/ede2dae3fbf5a161ba986dc72afc6877.jpg',
+                    size: 'large',
+                    id: 4,
+                    linkUrl: 'shop/womens'
+                },
+                {
+                    title: 'mens',
+                    imageUrl: 'https://static-01.daraz.pk/p/d14b1803339b1a266a196382bdfaf7c6.jpg',
+                    size: 'large',
+                    id: 5,
+                    linkUrl: 'shop/mens'
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {
+                    this.state.sections.map(({ title, imageUrl, id, size }) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default Directory;
