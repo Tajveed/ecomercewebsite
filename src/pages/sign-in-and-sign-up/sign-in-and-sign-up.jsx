9cebbbaabef3c1d726342b721fda11fb6a530fd7
unknown <Tajveed.Aslam@MKS.COM>
2019-11-07 15:25:55 +0500

    Sign In component added

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import './sign-in-and-sign-up.styles.scss';
+import SignIN from '../../components/SignIn/sign-in.component';
 
 const SignINSignUPPage = () => (
-
+    <div className='sign-in-and-sign-up'>
+        <SignIN />
+    </div>
 )
 
 export default SignINSignUPPage;
